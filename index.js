d3d98a243079352de16590ca0fe0979b4f23623e
Megan Dacus <meganjustice0609@gmail.com>
2023-03-14 11:12:14 +1100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Megan',
+    streetAddress: '401 Toorak Road',
+}
+
+function updateEmployeeWithKeyAndValue(employeeObj, key, value) {
+    const newEmployee = {...employeeObj, [key]: value}
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObj, key, value) {
+    employeeObj[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employeeObj, key) {
+    const deletedKey = {employeeObj}
+    return deletedKey
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeObj, key) {
+    delete employeeObj[key]
+    return employee
+}
